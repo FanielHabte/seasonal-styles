f92d04b58ba35550c403d2f67570580d6deb2263
FanielHabte <fanielhabte20@gmail.com>
2023-08-01 21:46:15 -0700

    update

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -3,19 +3,23 @@ $("document").ready(function(){
         e.preventDefault();
         
         if ($(this).attr("href") == "Summer"){
-            var season = $('p img').attr('src', 'images/summer-wear.jpg');
+            $("header img").attr('src', 'images/summer.gif');
+             $('p img').attr('src', 'images/summer-wear.jpg');
             $('html').css("background-color", "#EBA52B");
         }
         else if ($(this).attr("href") == "Spring"){
-            var season = $('p img').attr('src', 'images/spring-wear.jpg');
+            $("header img").attr('src', 'images/spring.gif');
+             $('p img').attr('src', 'images/spring-wear.jpg');
             $('html').css("background-color", "#2B7129");
         }
         else if ($(this).attr("href") == "Fall"){
-            var season = $('p img').attr('src', 'images/fall-wear.jpg');
+            $("header img").attr('src', 'images/fall.gif');
+            $('p img').attr('src', 'images/fall-wear.jpg');
             $('html').css("background-color", "#A81124");
         }
         else if ($(this).attr("href") == "Winter"){
-            var season = $("p img").attr('src', 'images/winter-wear.jpg');
+            $("header img").attr('src', 'images/winter.gif');
+            $("p img").attr('src', 'images/winter-wear.jpg');
             $('html').css("background-color", "#005393");
         }
         else{
